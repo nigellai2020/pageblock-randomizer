f3e5bd544550609da2425c739f9c7eef5ca3a183
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2023-02-21 14:39:03 +0800

    Apply rejection sampling to random numbers to address modulo bias

--- src/main/utils.ts
+++ src/main/utils.ts
@@ -7,24 +7,33 @@ async function hash(value: string) {
   return await window.crypto.subtle.digest("SHA-256", data);
 }
 
-async function mapRandomNumberToNumbers(seed: string, numberOfValues: number, from: number, to: number) {
-  const range = new BigNumber(to).minus(new BigNumber(from)).plus(1);
-  const partSize = range.idiv(numberOfValues);
+async function mapRandomNumberToNumbers(hexString: string, numberOfValues: number, from: number, to: number) {
   const parts: string[] = [];
-
-  for (let i = 0; i < numberOfValues; i++) {
-    const start = new BigNumber(from).plus(partSize.times(i));
-    const end = start.plus(partSize).minus(1);
-    const value = `${start.toFixed()},${end.toFixed()},${seed}`;
-    const hashedValue = await hash(value);
-    const bigNumber = new BigNumber('0x' + [...new Uint8Array(hashedValue)].map(x => x.toString(16).padStart(2, '0')).join(''));
-    const mappedNumber = start.plus(bigNumber.mod(partSize));
-    parts.push(mappedNumber.toFixed());
+  const range = new BigNumber(to).minus(new BigNumber(from)).plus(1);
+  if (range.lt(numberOfValues)) {
+    return [];
   }
 
-  return parts;
-}
+  const maxRandomValue = new BigNumber(2).pow(256).minus(1);
+  const rejectionThreshold = maxRandomValue.idiv(range).times(range);
+  let count = 0;
+  while (parts.length < numberOfValues) {
+    let randNum = new BigNumber(0);
+    const seed = `${count},${hexString}`;
+    const hashedValue = await hash(seed);
+    randNum = new BigNumber('0x' + [...new Uint8Array(hashedValue)].map(x => x.toString(16).padStart(2, '0')).join(''));
+    if (randNum.lt(rejectionThreshold)) {
+      randNum = randNum.mod(range);
+      const value = new BigNumber(from).plus(randNum).toFixed();
+      if (!parts.includes(value)) {
+        parts.push(value);
+      }
+    }
+    count++;
+  }
 
+  return parts.sort((a, b) => new BigNumber(a).minus(b).toNumber());
+}
 
 async function getResult(round: string, numberOfValues: number, from: number, to: number) {
   const drandResponse = await fetch(`${DRandAPI}${round}`);
